6324502cc7a3057e44ccbbcde0a7e09b03b2a613
kosuke <sheep.a08@gmail.com>
2019-09-30 23:37:13 +0900

    [課題G] ヘッダーの追記の実装
    関数を一つにして書き換えを行う方法

--- header_addtion.py
+++ header_addtion.py
@@ -4,42 +4,29 @@ import shutil
 import glob
 
 
-def rename_a():
-    data =[]
-    pass_list = glob.glob(os.path.join('score_data\school_a', '*'))
-    for f_pass in pass_list:
-        with open(f_pass, newline='', encoding='utf_8_sig') as f:
-            r = csv.reader(f)
-            for line in r:
-                data.append(line)
-        with open(f_pass, 'w', newline='', encoding='utf_8_sig') as f:
-            w = csv.writer(f)
-            w.writerow(['氏名', 'メールアドレス', '得点'])
-            w.writerows(data)
-
-
-def rename_b():
+def rename():
     data = []
-    pass_list = glob.glob(os.path.join('score_data\school_b', '*'))
-    for f_pass in pass_list:
-        fpass_list = glob.glob(os.path.join(f_pass, '*'))
-        for fpass in fpass_list:
-            with open(fpass, newline='', encoding='utf_8_sig') as f:
-                r = csv.reader(f)
-                for line in r:
-                    data.append(line)
-            with open(fpass, 'w', newline='', encoding='utf_8_sig') as f:
-                w = csv.writer(f)
-                w.writerow(['氏名', 'メールアドレス', '得点'])
-                w.writerows(data)
+    dir_list = glob.glob(os.path.join('score_data', '*'))
+    for dir_pass in dir_list:
+        f_pass_list = glob.glob(os.path.join(dir_pass, '*'))
+        for fpass in f_pass_list:
+            csv_pass_list = glob.glob(os.path.join(fpass, '*'))
+            for cpass in csv_pass_list:
+                with open(cpass, newline='', encoding='utf_8_sig') as f:
+                    r = csv.reader(f)
+                    for line in r:
+                        data.append(line)
+                with open(cpass, 'w', newline='', encoding='utf_8_sig') as f:
+                    w = csv.writer(f)
+                    w.writerow(['氏名', 'メールアドレス', '得点'])
+                    w.writerows(data)
 
 
 def main():
     if not os.path.exists("back_up_data"):
         shutil.copytree(".\score_data", "back_up_data")
 
-    rename_a()
-    rename_b()
+    rename()
 
 
 if __name__ == '__main__':
