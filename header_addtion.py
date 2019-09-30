0ab531b2e0cd0c73d69fb25cafbb2a4565ca8a16
kosuke <sheep.a08@gmail.com>
2019-10-01 00:22:36 +0900

    [課題G] ヘッダーの追記の実装
    バックアップしたデータから書き込み

--- header_addtion.py
+++ header_addtion.py
@@ -4,29 +4,33 @@ import shutil
 import glob
 
 
-def rename():
+def re_content(fpass):
+    backpass = fpass.replace('score_data', 'back_up_data')
     data = []
-    dir_list = glob.glob(os.path.join('score_data', '*'))
-    for dir_pass in dir_list:
-        f_pass_list = glob.glob(os.path.join(dir_pass, '*'))
-        for fpass in f_pass_list:
-            csv_pass_list = glob.glob(os.path.join(fpass, '*'))
-            for cpass in csv_pass_list:
-                with open(cpass, newline='', encoding='utf_8_sig') as f:
-                    r = csv.reader(f)
-                    for line in r:
-                        data.append(line)
-                with open(cpass, 'w', newline='', encoding='utf_8_sig') as f:
-                    w = csv.writer(f)
-                    w.writerow(['氏名', 'メールアドレス', '得点'])
-                    w.writerows(data)
+    with open(backpass, newline='', encoding='utf_8_sig') as f:
+        r = csv.reader(f)
+        for line in r:
+            data.append(line)
+    with open(fpass, 'w', newline='', encoding='utf_8_sig') as f:
+        w = csv.writer(f)
+        w.writerow(['氏名', 'メールアドレス', '得点'])
+        w.writerows(data)
+
+
+def add_header():
+    for school_pass in glob.glob(os.path.join('score_data', '*')):
+        for file_pass in glob.glob(os.path.join(school_pass, '*')):
+            if os.path.isfile(file_pass):
+                re_content(file_pass)
+            else:
+                for csv_pass in glob.glob(os.path.join(file_pass, '*')):
+                    re_content(csv_pass)
 
 
 def main():
     if not os.path.exists("back_up_data"):
         shutil.copytree(".\score_data", "back_up_data")
-
-    rename()
+    add_header()
 
 
 if __name__ == '__main__':
